c30837ea5cb8eed9d5502d1be7af740cef776aeb
Jacob Hummer <jcbhmr@outlook.com>
2025-10-12 01:18:03 +0000

    save

--- main.ts
+++ main.ts
@@ -2,11 +2,15 @@
 const pattern = new URLPattern({ pathname: "/:repo.git" });
 export default {
   fetch(request) {
-    const match = pattern.exec(request.url);
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      return Response.redirect("https://github.com/jcbhmr?tab=repositories", 307);
+    }
+    const match = pattern.exec(url);
     if (match == null) {
       return new Response(null, { status: 404 });
     }
-    const repo = match.pathname.groups;
-    return Response.redirect(`https://github.com/jcbhmr/${repo}.git`);
+    const repo = match.pathname.groups.repo!;
+    return Response.redirect(`https://github.com/jcbhmr/${repo}.git`, 307);
   },
 } satisfies Deno.ServeDefaultExport;
